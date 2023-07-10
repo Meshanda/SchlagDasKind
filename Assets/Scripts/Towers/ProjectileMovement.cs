011e64453a7a2cc0366e0c67dc91eb80789946f2
FranYuu <49402138+FranYuu@users.noreply.github.com>
2023-07-10 15:14:17 +0200

    Damage bullet ground work

--- Assets/Scripts/Towers/ProjectileMovement.cs
+++ Assets/Scripts/Towers/ProjectileMovement.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 public class ProjectileMovement : MonoBehaviour
 {
     private Collider2D _target;
-    public float _speed;
-    public float _damage;
+    public float Speed;
+    public float Damage;
 
     public void Update()
     {
         Quaternion rotation = Quaternion.LookRotation
             (_target.transform.position - transform.position, transform.TransformDirection(Vector3.up));
         transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
-        transform.position += (_target.transform.position - transform.position).normalized * _speed;
+        transform.position += (_target.transform.position - transform.position).normalized * Speed;
     }
 
     public void SetTarget(Collider2D target)
     {
         _target = target;
     }
+
+    public void SetBulletProperties(float speed, float damage) 
+    {
+        Speed = speed;
+        Damage = damage;
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (_target != collision)
             return;
+        //collision.GetComponent<>().DoDamage(Damage)
+
         Destroy(gameObject);
     } 
 }
