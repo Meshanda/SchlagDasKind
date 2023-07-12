67bc09a907818ada97a6b311174336c88d589f29
Lionel <li.janin@laposte.net>
2023-07-12 10:38:22 +0200

    add sprite converter + fix sprite creation

---
+++ Assets/Scripts/Utils/SpriteConverter.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class SpriteConverter
+{
+    public static async Task<Sprite> CreateSpriteFromPath(string filePath)
+    {
+        Texture2D tex2D;
+        Sprite outSprite;
+
+        if (File.Exists(filePath)){
+            var fileData = await File.ReadAllBytesAsync(filePath);
+            tex2D = new Texture2D(2, 2);
+            
+            if (tex2D.LoadImage(fileData))
+            {
+                Texture2D spriteTexture = tex2D;
+                outSprite = Sprite.Create(spriteTexture, new Rect(0, 0, spriteTexture.width, spriteTexture.height),new Vector2(0.5f,0.31f));
+
+                Debug.Log("Sprite created");
+                return outSprite;
+            }
+        }
+        return null;
+    }
+}
