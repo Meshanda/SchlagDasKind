24dbe0b26dc7fc72b1acde1cb125d6fc071201b9
Killian <32911550+Meshanda@users.noreply.github.com>
2023-07-12 15:53:14 +0200

    starting money

--- Assets/Scripts/Monnaie/MoneySystem.cs
+++ Assets/Scripts/Monnaie/MoneySystem.cs
@@ -19,6 +19,7 @@ namespace Monnaie
         private void Start() 
         {
             _moneySO.value = StartingGold;
+            UpdateMoney?.Invoke(_moneySO.value);
         }
 
         private void OnEnable()
